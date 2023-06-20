78f0a9984b25e16ce4f572a3c3de7d16aa354e48
jeremdh62 <jdhaille@myges.fr>
2023-06-20 20:16:18 +0200

    add compression

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     logger: ['error', 'warn', 'debug'],
   });
   app.enableCors();
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
